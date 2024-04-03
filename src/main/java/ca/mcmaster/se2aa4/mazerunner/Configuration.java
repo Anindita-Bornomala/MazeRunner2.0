8645bb8c7524ab55c8f61724065327798f72d0ea
Anindita <anindita.bornomala@gmail.com>
2024-04-03 03:03:40 -0400

    Added new classes Compass and Direction for refactoring

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Configuration.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Configuration.java
@@ -6,28 +6,36 @@ import org.apache.commons.cli.*;
 public class Configuration {
     private final String inputFile;
     private final String pathGuess;
+    private final String method;
 
-    public Configuration(String inputFile, String pathGuess) {
+    public Configuration(String inputFile, String pathGuess, String inputMethod) {
         if (inputFile == null) {
             throw new IllegalArgumentException("Maze text file is empty!");
         }
         this.inputFile = inputFile;
         this.pathGuess = pathGuess;
+        this.method = inputMethod;
     }
 
     public static Configuration configure(String[] cmdArgs) throws Exception, FileNotFoundException {
         Options options = new Options();
         options.addOption("i", "input", true, "Input filepath");
         options.addOption("p", "pathGuess", true, "Input path guess");
+        options.addOption("m", "method", true, "Input method");
         
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, cmdArgs);
         String inputFilePath = cmd.getOptionValue("i");
         String inputPathGuess = cmd.getOptionValue("p");
-        return new Configuration(inputFilePath, inputPathGuess);
+        String inputMethod = cmd.getOptionValue("method");
+        return new Configuration(inputFilePath, inputPathGuess, inputMethod);
     }
 
     public String getInputFile() { return inputFile; }
 
     public String getPathGuess() { return pathGuess; }
+
+    public String getMethod() { return method; }
 }
+
+// We'll need to consider a new flag, one that allows the user to choose their preferred algorithm.
---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
@@ -0,0 +1,30 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public enum Direction {
+    NORTH('N'),
+    SOUTH('S'),
+    EAST('E'),
+    WEST('W');
+
+    private final char dir;
+
+    Direction(char dir) { this.dir = dir; } // constructor
+
+    @Override
+    public String toString() { return "" + dir; }
+
+    public static Direction toDirection(String dir) {
+        switch (dir.toUpperCase()) {
+            case "N":
+                return NORTH;
+            case "S":
+                return SOUTH;
+            case "E":
+                return EAST;
+            case "W":
+                return WEST;
+            default:
+                throw new IllegalArgumentException("Invalid direction: " + dir);
+        }
+    }
+}
