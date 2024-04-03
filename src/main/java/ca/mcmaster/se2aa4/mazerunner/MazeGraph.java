11048d1586e02b24dac04fbc59bf108ac2c34ec6
Anindita <anindita.bornomala@gmail.com>
2024-04-03 03:57:50 -0400

    Refactored PathFinder methods into MazeGraph

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeGraph.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeGraph.java
@@ -10,6 +10,49 @@ public class MazeGraph {
     private MazeData maze; // delete later
 
 
+
+    public boolean checkNorth(MazeData mase, Coordinate currentSteps, Coordinate nextStep, Direction direction) {
+        char element = maze.getMazeElement(nextStep.getX(), nextStep.getY());
+        if (element != '#') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkFront(MazeData maze, Integer[] currentSteps, char direction) { // check for walls in front
+        Integer row = currentSteps[0];
+        Integer col = currentSteps[1];
+        if (direction == 'E') {
+            col++;
+        } else if (direction == 'S') {
+            row++;
+        } else if (direction == 'W') {
+            col--;
+        } else {
+            row--;
+        }
+        if (maze.getMazeElement(row, col) != '#') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    
+    
+    public Coordinate nextStep(Coordinate currentStep, Direction direction) { // determine direction
+        Coordinate nextPosition = currentStep;
+        nextPosition.moveForward(direction);
+        return nextPosition;
+    }
+
+
+    public Coordinate moveForward(Coordinate currentStep, Coordinate nextStep) { // move forward
+        currentStep = nextStep;
+        return currentStep;
+    }
+
     public Coordinate pathStart(MazeData maze) { // get start coordinates
         this.maze = maze; // delete later
         Coordinate startC = new Coordinate(0, 0);
