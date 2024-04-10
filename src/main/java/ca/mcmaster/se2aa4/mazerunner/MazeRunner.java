9dd55beddd5db8cade23a1ead33e0a78bf207c88
Anindita <anindita.bornomala@gmail.com>
2024-04-09 22:31:43 -0400

    Cleaned up unused lines in MazeRunner, tested changes for successful and accurate execution

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeRunner.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeRunner.java
@@ -5,16 +5,13 @@ import org.apache.logging.log4j.Logger;
 
 public class MazeRunner {
     private static final Logger logger = LogManager.getLogger();
-    private static String path = "";
 
     public MazeRunner(String[] args) {
-
         try {
             Configuration config = Configuration.configure(args);
 
             logger.info("** Starting Maze Runner");
-            long startTime = System.currentTimeMillis(); // LOAD MAZE TIME
-            // System.out.println(startTime); // TEST
+            long startTime = System.currentTimeMillis(); // BENCHMARK 1
             
             logger.info("**** Reading the maze from file " + config.getInputFile());
             logger.info("**** Reading for pathGuess: " + config.getPathGuess());
@@ -24,12 +21,12 @@ public class MazeRunner {
             // mazeInput.printMaze(); // TEST
 
             long loadTime = System.currentTimeMillis() - startTime;
-            // logger.info("Time spent loading the maze: " + String.format("%.2f", loadTime / 1000.0) + " seconds");
 
             System.out.println(); // System.out.print(System.lineSeparator());
             logger.info("**** Computing path");
 
 
+            // BASELINE MODE CODE
             if (config.getBaseline() != null) {
                 // benchmark 1
                 logger.info("Time spent loading the maze: " + String.format("%.2f", loadTime / 1000.0) + " seconds");
@@ -48,35 +45,19 @@ public class MazeRunner {
                 righthand.rightHandRule();
                 // Integer baselineCount = righthand.getPathCount();
                 long baselineTime = System.currentTimeMillis() - startTime;
-                logger.info("Time spent running BFS algorithm: " + String.format("%.2f", baselineTime / 1000.0) + " seconds");
+                logger.info("Time spent running Righthand algorithm: " + String.format("%.2f", baselineTime / 1000.0) + " seconds");
                 
                 // benchmark 4
-
-                // String path = "lit";
-                // Integer baselineCount = path.length();
-                // Integer methodCount = path.length();
 
                 logger.info(righthand.getPathCount());
                 logger.info(bfs.getPathCount());
                 double speedup = (double) righthand.getPathCount() / bfs.getPathCount();
                 logger.info("Improvement on the path as a speedup (BFS vs Righthand Rule): " + String.format("%.2f", speedup));
-                
-                // Make all the benchmarks happen here
-                // benchmark1: capture time between load operations
-                // benchmark 2: capture time in rightHandRule class and return it
-                // benchmark 3: capture time in BFS class and return it
-                // benchmark 4: make difference happen in baseline mode (MazeRunner class)
-
-                String yippee = config.getBaseline();
-                logger.info("**** Baseline: " + config.getBaseline());
-            } else {
-                logger.info("**** Baseline not provided");
-                Integer yippee2 = 0;
             }
+            
+           
 
-            if (config.getBaseline() != null) { // baseline mode
-                System.out.println("EPIC");
-            } else if (config.getPathGuess() != null && config.getBaseline() == null) { // check path
+            if (config.getPathGuess() != null && config.getBaseline() == null) { // check path
                 PathChecker check = new PathChecker(mazeInput);
                 System.out.println(check.pathCheck(mazeInput, config.getPathGuess()));
             } else if (config.getPathGuess() == null && config.getBaseline() == null) {
@@ -99,10 +80,6 @@ public class MazeRunner {
         logger.info("** End of MazeRunner");
     }
 }
-
-// This should be turned into an interface that runs either algorithm, RightHandRule or BFS 
-
-
 
 /*
 Here's the gist:
